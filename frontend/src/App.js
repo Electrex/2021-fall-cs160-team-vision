143ffc9b9e0a23f38881963b4b341027d4d84977
sage-suarez <sagesuarez6@gmail.com>
2021-10-08 19:19:04 -0700

    Revert "Added preliminary login page"
    This reverts commit d6ccaf5a5e152ddc7724efa59574d1e8ef2e2e70.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Register from './components/Register'
-import Login from './components/Login'
 import UserLanding from './components/UserLanding';
 
 function App(){
@@ -10,7 +9,6 @@ function App(){
       <main>
         <Switch>
           <Route path='/' exact component={Register}/>
-          <Route path='/login' exact component={Login}/>
           <Route path='/me' exact component={UserLanding}/>
         </Switch>
       </main>
