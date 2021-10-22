b1341475574560f27e630b19f8802c03d5e86ed7
Dylan Nguyen <dylan.c.nguyen@sjsu.edu>
2021-10-22 11:18:34 -0700

    Added method to dos earch request

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+const search = async (q) => {
+    try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+
+        const body = {'name': q};
+
+        const res = await axios.post('/api/profile/byname', body, config);
+        clearState();
+        if (res.status === 400 || res.status === 500){
+          return {}
+        }
+        console.log(res.data);
+        return res.data
+      } catch (error) {
+        console.log(error.response.data);
+        return {}
+      }
+}
+
 function UserList(props) {
     const history = useHistory();
     const [query, setQuery] = useState('');
     const searchQuery = props.location.search.substring(1)
+    const searchResult = search(searchQuery)
 
     const handleSearch = (query) => {
         history.push(`/users?${query}`);
@@ -25,7 +49,7 @@ function UserList(props) {
             <button onClick={(e) => handleSearch(query)}>Search</button>
             </div>
             <div>
-                {searchQuery}
+                {searchResult}
             </div>
         </div>
     );
