ede3512aa376dd1613b942cc27556693b6ba8be7
Gurveer Singh <70867602+Gurv33r@users.noreply.github.com>
2021-10-24 16:32:37 -0700

    Update CreateReview.js

--- frontend/src/components/CreateReview.js
+++ frontend/src/components/CreateReview.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 //import { useHistory } from 'react-router-dom';
 import axios from 'axios';
-import e from 'express';
+// import e from 'express';
 
 function CreateReview(props) {
     // const history = useHistory();
