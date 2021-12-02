fcb1a98cfae5f73202614318609704a1614bc0d2
Dylan Nguyen <dylan.c.nguyen@sjsu.edu>
2021-12-01 17:45:16 -0800

    docker stuff

--- frontend/src/components/UserLanding.js
+++ frontend/src/components/UserLanding.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 import axios from 'axios';
 import { useHistory, NavLink } from 'react-router-dom';
 import UserList from './UserList';
